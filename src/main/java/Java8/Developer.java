de3e089e6236a8452388edd571a3864edfeb5ff6
Stepan Morgachev <stepan.morgachev@gmail.com>
2023-10-01 11:10:01 +0300

    Добавил решение задания про стримы

---
+++ src/main/java/Java8/Developer.java
@@ -0,0 +1,41 @@
+package Java8;
+
+import java.util.List;
+
+/**
+ * @author Stepan Morgachev
+ * @date 01.10.2023 10:37
+ */
+public class Developer {
+    private String name;
+    private List<String> languages;
+
+    public Developer(String name, List<String> languages) {
+        this.name = name;
+        this.languages = languages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "name='" + name + '\'' +
+                ", languages=" + languages +
+                '}';
+    }
+}
---
+++ src/main/java/Java8/Solution.java
@@ -0,0 +1,36 @@
+package Java8;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Stepan Morgachev
+ * @date 01.10.2023 10:37
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Developer dev1 = new Developer("Наташа", Arrays.asList("Java", "C++"));
+        Developer dev2 = new Developer("Эрнест", Arrays.asList("Java", "Python"));
+        Developer dev3 = new Developer("Элла", Arrays.asList("С#", "Python", "JavaScript"));
+        Stream<Developer> developerStream = Stream.of(dev1, dev2, dev3);
+
+        List<Developer> developersWithUniqueLanguages = developerStream
+                .flatMap(dev -> dev.getLanguages().stream().map(lang -> new AbstractMap.SimpleEntry<>(dev, lang)))
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())
+                ))
+                .values()
+                .stream()
+                .filter(developerList -> developerList.size() == 1)
+                .distinct()
+                .flatMap(List::stream).toList();
+
+        for(Developer s: developersWithUniqueLanguages)
+            System.out.println(s);
+    }
+}
